8dc12860a084c07cef99f5bf08c180b278342741
MichaelDongmo <momogardel@gmail.com>
2021-02-06 13:19:43 +0100

    20210206 created home page banner.

--- src/components/home/Banner.vue
+++ src/components/home/Banner.vue
@@ -1,96 +1,108 @@
 <template>
- 
-<div class=" wpb_column vc_column_container vc_col-sm-12">
-<div class="wpb_wrapper">
-<div class="fw-section hb-fw-599b22a654ae9 without-border no-overlay light-style">
- 
- <div class="bg_banner row justify-center">
-   <div class="banner_content">
-    <div class="v1 vc_col-sm-3 nbm">
-      <div class="learning-automated-div">
-        <span style="color:#FFFFFF;">We help you to create<br>the future that you want.
-        <br>Making your lives simplier, cheaper and easier</span>
+  <div class="bg_banner row justify-center">
+    <div class="banner_content">
+      <div class="vc_col-sm-3 nbm">
+        <div class="learning-automated-div">
+          <span style="color: #ffffff"
+            >We help you to create<br />the future that you want. <br />Making your lives
+            simplier, cheaper and easier</span
+          >
+        </div>
+        <div class="learning-mission row">
+          <q-btn flat class="col-1" icon="fa fa-circle" color="white" />
+          <span class="col-10" style="color: #ffffff">{{ company.content_1 }}</span>
+          <q-btn flat class="col-1" icon="fa fa-circle" color="white" />
+        </div>
+        <div style="text-align: center">
+          <q-btn color="secondary" label="Read more" to="/about" />
+        </div>
       </div>
-      <div class="learning-mission row">
-        <q-btn flat class="col-1" icon="fa fa-circle" color="white" />
-          <span  class="col-10" style="color:#FFFFFF;">{{company.content_1}}</span>
-        <q-btn flat class="col-1" icon="fa fa-circle" color="white" />
-      </div>
-      <div style="text-align: center;">
-        <q-btn color="secondary" label="Read more" to="/about" />
-      </div>
-
-      <BannerChild />
     </div>
+    <div class="my-card">
+      <q-banner class="bg-grey-3">
+        <template v-slot:avatar>
+          <div class="image">
+            <img src="~assets/sysait_ecran.png" style="width: 200px; height: 200px" />
+          </div>
+        </template>
+        <div id="about">
+          <font
+            style="text-decoration: underline"
+            face="Time new roman"
+            class="sysait_black"
+            size="6"
+          >
+            <b>Who are we</b> </font
+          ><br /><br /><br />
+          <font face="Time new roman" class="sysait_black" size="4">
+            <p>{{ company.description }}</p>
+          </font>
+        </div>
+        <div style="text-align: left">
+          <q-btn color="secondary" label="Find out more" to="/about" />
+        </div>
+      </q-banner>
     </div>
   </div>
-
-</div></div></div>
 </template>
 
 <script>
-import BannerChild from '@/components/BannerChild.vue'
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
 export default {
-  name: 'Banner',
-  
-  components: {
-    BannerChild
+  name: "Banner",
+
+  components: {},
+
+  data() {
+    return {};
   },
 
-  data () {
-    return {
-    }
-  },
-
-   methods: {
-   
-  },
+  methods: {},
 
   computed: {
-    ...mapGetters(
-      [
-        'company',
-        'langCange'
-      ]),
-  }
-}
+    ...mapGetters(["company", "langCange"]),
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+.bg_banner {
+  background-color: #cccccc;
+  background-image: url("~assets/HomePageImage.png");
+  background-size: cover;
+  padding-top: 10px;
+  padding-bottom: 100px;
+  background-repeat: no-repeat;
+}
 
-  .bg_banner {
-    background-color:#cccccc;
-    background-image:url("~assets/HomePageImage.png");
-    background-size:cover;
-    padding-top:40px;
-    padding-bottom:100px;
-    background-repeat: no-repeat;
-  }
+.learning-automated-div {
+  text-align: center;
+  font-size: 48px;
+  line-height: 52px;
+  padding: 100px 5px 10px 5px;
+  font-weight: bold;
+}
 
-  .learning-automated-div {
-    text-align: center; font-size: 48px;
-    line-height: 52px; padding: 100px 5px 10px 5px;
-    font-weight: bold;
-  }
+.banner_content {
+  text-align: center;
+  width: 75%;
+  padding: 5px 40px 5px;
+  border-right: 6px solid rgb(234, 237, 240);
+  border-left: 6px solid rgb(234, 237, 240);
+  height: 300px;
+}
 
-  .v1 {
-    border-right:  6px solid rgb(234, 237, 240);
-    border-left:   6px solid  rgb(234, 237, 240);
-    height: 300px;
-  }
+.my-card {
+  width: 75%;
+  max-width: 1200px;
+  padding-bottom: 0;
+  padding-top: -200px;
+  transform: translateY(250px);
+  border: 1px;
+}
 
-  .learning-mission {
-    text-align: center;
-    font-size: 18px;
-    line-height: 22px;
-    padding: 0 5px 40px 5px;
-  }
-
-  .banner_content {
-    text-align: center;
-    width: 75%;
-    padding: 0 5px 40px 5px;
-  }
-
+#about {
+  position: right;
+  color: black;
+}
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,31 +1,29 @@
 <template>
   <q-page class="flex-center row">
-   <Banner class="col-12"/>
-   <Technologies class="col-12"/>
-   <Planing class="col-12"/>
+    <Banner class="col-12" />
+    <Technologies class="col-12" />
+    <Planing class="col-12" />
   </q-page>
 </template>
 
 <script>
-import Banner from '@/components/home/Banner.vue'
-import Technologies from '@/components/home/Technologies.vue'
-import Planing from '../components/home/Planing.vue'
+import Banner from "@/components/home/Banner.vue";
+import Technologies from "@/components/home/Technologies.vue";
+import Planing from "../components/home/Planing.vue";
 
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
 
 export default {
-  name: 'PageIndex',
+  name: "PageIndex",
   components: {
     Banner,
     Technologies,
     Planing,
   },
   computed: {
-    ...mapGetters(
-      [
-        'copmany',
-
-      ]),
+    ...mapGetters(["copmany"]),
   },
-}
+};
 </script>
+
+<style scoped lang="scss"></style>
