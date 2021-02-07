d70e18161eee697ee8b6b21de73d0a169cbdd248
wilfried ndomo kenfack <wilfried.ndomo@gmail.com>
2021-02-07 18:29:05 +0100

    add padding on css

--- src/components/home/Technologies.vue
+++ src/components/home/Technologies.vue
@@ -8,19 +8,19 @@
        <div class="row q-my-lg">
         <div class="col-md-3 col-sm-6 col-xs-12 q-pb-lg" v-for= "tecnologie in tecnologies">
             <div class="text-info " style="font-size: 2em">
-             <q-icon :name= tecnologie.icon class="color_sysait_cerulean" />
+             <q-icon :name="tecnologie.icon"  class="color_sysait_cerulean" />
             </div>
              <div class="text-h7 text-bold">{{ tecnologie.name}}</div>
             <div>{{ tecnologie.content}}</div>
 
         </div>
-       
-    </div>
-        
-  
 
-        
-    
+    </div>
+
+
+
+
+
     </q-page>
   </q-page-container>
 
@@ -42,49 +42,49 @@ export default {
 						id: 1,
 						name: 'Futur',
 						icon: "fa fa-arrow-up",
-						content: 'Artificial inteligent,Maching learning,...'						
+						content: 'Artificial inteligent,Maching learning,...'
 					},
 					{
 						id: 2,
 						name: 'Framework',
 						icon: "fa fa-retweet",
-						content: 'Ruby On Rails,Spring,Angular JS,Jquery,Bootstrap,...'	
+						content: 'Ruby On Rails,Spring,Angular JS,Jquery,Bootstrap,...'
 					},
 					{
 						id: 3,
 						name: 'Coding',
 						icon: "fa fa-code",
-						content: 'Ruby,HTML,CSS,JAVA,C,...'	
+						content: 'Ruby,HTML,CSS,JAVA,C,...'
                     },
                     {
 						id: 4,
 						name: 'Database',
 						icon: "fa fa-database",
-						content: 'MySql,SQlite3,DB2,Oracle,SQL Server,...'	
+						content: 'MySql,SQlite3,DB2,Oracle,SQL Server,...'
                     },
                      {
 						id: 5,
 						name: 'Mobile appications',
 						icon: "fa fa-mobile",
-						content: ''	
+						content: ''
              },
             {
 						id: 6,
 						name: 'Web appications',
 						icon: "fa fa-desktop",
-						content: ''	
+						content: ''
            },
             {
 						id: 7,
 						name: 'Projects management',
 						icon: "groups",
-						content: ''	
+						content: ''
           },
            {
 						id: 8,
 						name: 'Customized software',
 						icon: "last_page",
-						content: ''	
+						content: ''
 					}
 				]
     }
