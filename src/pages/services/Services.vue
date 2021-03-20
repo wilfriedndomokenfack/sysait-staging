ebeefa6fa2df6a536db1a82eac83abbd3150ae70
MichaelDongmo <momogardel@gmail.com>
2021-03-20 13:22:28 +0100

    20210320 fix error service page
    20210320 fix error service page

--- src/pages/services/Services.vue
+++ src/pages/services/Services.vue
@@ -7,36 +7,8 @@
         :companyName="companyName"
       />
 
-      <ServicesComponent
-        v-if="services.lenght > 0"
-        :propServices="services"
-        :key="myKey"
-      />
-      <EmptyComponent v-else-if="services.lenght == 0" />
-      <div class="q-pa-md q-gutter-sm">
-        <q-btn label="Click me" color="primary" @click="myAlert = true" />
-
-        <q-dialog
-          v-model="myAlert"
-          persistent
-          transition-show="scale"
-          transition-hide="scale"
-        >
-          <q-card class="bg-teal text-white" style="width: 300px">
-            <q-card-section>
-              <div class="text-h6">Persistent</div>
-            </q-card-section>
-
-            <q-card-section class="q-pt-none">
-              Click/Tap on the backdrop.
-            </q-card-section>
-
-            <q-card-actions align="right" class="bg-white text-teal">
-              <q-btn flat label="OK" v-close-popup />
-            </q-card-actions>
-          </q-card>
-        </q-dialog>
-      </div>
+      <ServicesComponent :propServices="services" :key="myKey" />
+      <EmptyComponent v-if="services.length == 0" />
     </div>
   </q-page>
 </template>
