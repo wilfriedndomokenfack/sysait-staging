243a61c5430aa6ae0da34a71189ce12381b57cbb
Rose <diamant24carats@gmail.com>
2021-03-27 10:36:47 +0100

    Implementation header of product page

---
+++ src/components/productPage/ProductPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>My component</div>
+</template>
+
+<script>
+export default {
+  name: "ProductPage",
+  props: ["propProduct"],
+  data() {
+    return {};
+  }
+};
+</script>
--- src/pages/products/Product.vue
+++ src/pages/products/Product.vue
@@ -1,13 +1,38 @@
 <template>
   <q-page padding>
     <div>
-      <h5>Product page</h5>
+      <BannerPages
+        :bannerUrl="bannerUrl"
+        :pageName="pageName"
+        :companyName="companyName"
+      />
     </div>
   </q-page>
 </template>
 
 <script>
+import BannerPages from "@/components/utils/BannerPages.vue";
 export default {
-name: 'Product',
-}
+  name: "Product",
+  components: {
+    BannerPages
+  },
+  data() {
+    return {
+      bannerUrl: null,
+      pageName: null,
+      companyName: null,
+      myKey: 0
+    };
+  },
+  async mounted() {
+    /*  if (!this["rosine/services"]) {
+      await this.getServices();
+    } 
+    this.services = this["michael/services"];*/
+    this.bannerUrl = "ImageProducts.png";
+    this.pageName = "Product";
+    this.companyName = this.company.denomination;
+  }
+};
 </script>
