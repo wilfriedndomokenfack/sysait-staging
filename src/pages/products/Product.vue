290225ae5fc360ec8047f905cca1ebb03667055e
Rose <diamant24carats@gmail.com>
2021-03-28 09:20:07 +0200

    get a product from the store

--- src/pages/products/Product.vue
+++ src/pages/products/Product.vue
@@ -7,11 +7,18 @@
         :companyName="companyName"
       />
     </div>
+    <div>
+      ProductId:{{productId}}<br>
+      {{product.description}}
+      
+       
+    </div>
   </q-page>
 </template>
 
 <script>
 import BannerPages from "@/components/utils/BannerPages.vue";
+import { mapGetters } from "vuex";
 export default {
   name: "Product",
   components: {
@@ -22,17 +29,40 @@ export default {
       bannerUrl: null,
       pageName: null,
       companyName: null,
-      myKey: 0
+      productId: this.$route.params.product_id,
+      products:null,
+      product:null,
+
+
     };
   },
+  computed: {
+    ...mapGetters(["michael/products", "company"])
+  },
   async mounted() {
-    /*  if (!this["rosine/services"]) {
-      await this.getServices();
-    } 
-    this.services = this["michael/services"];*/
+    if (!this["michael/products"]) {
+      this.$router.push({ name : "home" });
+      
+    }
+    this.products = this["michael/products"];
+    this.getProduct()
     this.bannerUrl = "ImageProducts.png";
     this.pageName = "Product";
     this.companyName = this.company.denomination;
-  }
+    
+    
+  },
+   methods: {
+    getProduct() {
+     
+      
+      this.product= this.products.filter(v => v.id === this.productId)[0];
+      if(!this.product){
+         this.$router.push({ name: "products" });
+
+      }
+
+    },
+  },
 };
 </script>
